2aaaa45b930f82a7a45ae560c6a961092cc8e4b1
proshanto7 <proshantosarker112@gmail.com>
2025-12-21 03:19:32 +0600

    clear all Button Add

--- src/component/todoList/TodoList.jsx
+++ src/component/todoList/TodoList.jsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { MdDeleteForever } from "react-icons/md";
-import { getDatabase, ref, set, push, onValue } from "firebase/database";
+import {
+  getDatabase,
+  ref,
+  set,
+  push,
+  onValue,
+  remove,
+} from "firebase/database";
 
 function TodoList() {
   const [task, setTask] = useState("");
   const [listItem, setListItem] = useState([]);
+
   const db = getDatabase();
   const handleInputValue = (e) => {
     setTask(e.target.value);
@@ -40,28 +48,30 @@ function TodoList() {
       const arrey = [];
 
       snapshot.forEach((item) => {
-        arrey.push({...item.val(), id:item.key});
+        arrey.push({ ...item.val(), id: item.key });
       });
 
       setListItem(arrey);
     });
   }, []);
 
-  // remove list function 
+  // remove list function
 
-  const handleRemoveBtn = (id) =>{
+  const handleRemoveBtn = (id) => {
+    remove(ref(db, "todoList/" + id));
+  };
 
+  // clear all btn function
 
+  const handleClearAllBtn = () => {
+    remove(ref(db, "todoList/"));
+  };
 
-
-  }
-
-  console.log(listItem);
   return (
-    <section className="bg-gray-400 w-full h-screen ">
-      <div className="p-4 max-w-80  mx-auto bg-white">
+    <section className=" w-full h-full p-2 ">
+      <div className="p-4  max-w-80  mx-auto bg-white rounded-xl">
         <h2 className="text-center font-bold font-serif mb-5 text-2xl">
-          My ToDo List
+          My ToDo-List
         </h2>
 
         <form onSubmit={handleFormSubmit}>
@@ -75,7 +85,7 @@ function TodoList() {
 
           <button
             onClick={handleTodoBtn}
-            className="bg-green-500 w-full text-lg font-serif text-white font-semibold capitalize rounded-2xl "
+            className="bg-green-500 w-full text-lg font-serif text-white font-semibold capitalize rounded-2xl cursor-pointer hover:bg-yellow-500 duration-300 "
           >
             add task
           </button>
@@ -83,14 +93,33 @@ function TodoList() {
 
         <div className="mt-4">
           <ul>
-            {listItem.map((item) => (
-              <li key={item.id} className="flex items-center justify-between text-[16px] font-semibold capitalize  px-3 py-1 rounded-2xl shadow-input mb-3 ">
-                <span>{item.listName}  </span>
+            {listItem.map((item, index) => (
+              <li
+                key={item.id}
+                className="flex items-center justify-between text-[16px] font-semibold capitalize  px-3 py-1 rounded-2xl shadow-input mb-3 "
+              >
+                <span>
+                  {index + 1}. {item.listName}{" "}
+                </span>
 
-                <MdDeleteForever onClick={()=> handleRemoveBtn (item.id)} className=" rounded-2xl text-lg hover:text-white hover:bg-red-500 duration-300 cursor-pointer" />
+                <MdDeleteForever
+                  onClick={() => handleRemoveBtn(item.id)}
+                  className=" rounded-2xl text-lg hover:text-white hover:bg-red-500 duration-300 cursor-pointer"
+                />
               </li>
             ))}
           </ul>
+
+          {listItem.length > 1 && (
+            <div className="flex items-center justify-center">
+              <button
+                onClick={handleClearAllBtn}
+                className=" bg-green-500 py-1 px-4 text-white font-semibold font-serif rounded-2xl hover:bg-red-500 duration-500 cursor-pointer mx-auto "
+              >
+                Clear All
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </section>
