1e8baf2b1f1097dce5002e2ab589c1fa1c8a05b7
proshanto7 <proshantosarker112@gmail.com>
2025-12-24 02:35:13 +0600

    data update function done

--- src/component/todoList/TodoList.jsx
+++ src/component/todoList/TodoList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { MdDeleteForever } from "react-icons/md";
+import { MdDeleteForever, MdCancelPresentation } from "react-icons/md";
+import { CiEdit } from "react-icons/ci";
 import {
   getDatabase,
   ref,
@@ -7,11 +8,15 @@ import {
   push,
   onValue,
   remove,
+  update,
 } from "firebase/database";
 
 function TodoList() {
   const [task, setTask] = useState("");
   const [listItem, setListItem] = useState([]);
+  const [updateList, setUpdateList] = useState("");
+  const [edit, setEdit] = useState(false);
+  const [editId, setEditId] = useState(null);
 
   const db = getDatabase();
   const handleInputValue = (e) => {
@@ -31,12 +36,10 @@ function TodoList() {
       return;
     }
 
-    console.log(`add item ${task}`);
-    setTask("");
-
     set(push(ref(db, "todoList/")), {
       listName: task,
     });
+    setTask("");
   };
 
   // read data fireBase
@@ -67,9 +70,28 @@ function TodoList() {
     remove(ref(db, "todoList/"));
   };
 
+  // data update
+
+  const handleUptedeBtn = (id) => {
+    setEditId(id);
+    setEdit(!edit);
+  };
+
+  const Update = () => {
+    update(ref(db, "todoList/" + editId), {
+      listName: updateList,
+    }).then(() => {
+      setEdit(false);
+    });
+  };
+
+  // update div cancle btn
+  const handleCancelBtn = () => {
+    setEdit(false);
+  };
   return (
     <section className=" w-full h-full p-2 ">
-      <div className="p-4  max-w-80  mx-auto bg-white rounded-xl">
+      <div className="p-4 relative max-w-180  mx-auto bg-white rounded-xl">
         <h2 className="text-center font-bold font-serif mb-5 text-2xl">
           My ToDo-List
         </h2>
@@ -102,10 +124,17 @@ function TodoList() {
                   {index + 1}. {item.listName}{" "}
                 </span>
 
-                <MdDeleteForever
-                  onClick={() => handleRemoveBtn(item.id)}
-                  className=" rounded-2xl text-lg hover:text-white hover:bg-red-500 duration-300 cursor-pointer"
-                />
+                <div className="flex gap-4 items-center">
+                  <CiEdit
+                    onClick={() => handleUptedeBtn(item.id)}
+                    className=" rounded-2xl text-lg hover:text-white hover:bg-green-600 duration-300 cursor-pointer"
+                  />
+
+                  <MdDeleteForever
+                    onClick={() => handleRemoveBtn(item.id)}
+                    className=" rounded-2xl text-lg hover:text-white hover:bg-red-500 duration-300 cursor-pointer"
+                  />
+                </div>
               </li>
             ))}
           </ul>
@@ -121,6 +150,31 @@ function TodoList() {
             </div>
           )}
         </div>
+
+        {edit && (
+          <div className="bg-gray-400/90 absolute top-0 left-0 w-full h-full  flex items-center p-10">
+            <button
+              onClick={handleCancelBtn}
+              className="text-3xl absolute top-1 right-1 bg-white p-1 rounded-full hover:text-red-600 duration-300 cursor-pointer"
+            >
+              <MdCancelPresentation />
+            </button>
+
+            <input
+              onChange={(e) => setUpdateList(e.target.value)}
+              type="text"
+              placeholder="Update Your List"
+              className="bg-white w-full  rounded-s-2xl px-2 py-3   outline-none shadow-input "
+            />
+
+            <button
+              onClick={Update}
+              className="bg-green-500  text-lg font-serif text-white font-semibold capitalize rounded-e-2xl cursor-pointer hover:bg-yellow-500 duration-300 px-4 py-2.5 "
+            >
+              Upadte
+            </button>
+          </div>
+        )}
       </div>
     </section>
   );
